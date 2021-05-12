189d5387f3408132a4e7ad10418aa5dee3d5f40b
Bruno Garcia Rana <brunogarciarana@gmail.com>
2021-05-12 20:25:39 -0300

    feat: configuration improved

--- src/main/java/com/demo/kafkapubsub/config/AppConfig.java
+++ src/main/java/com/demo/kafkapubsub/config/AppConfig.java
@@ -5,5 +5,5 @@ public class AppConfig {
     private AppConfig() { }
 
     public static final String TOPIC_NAME = "t-topic-name";
-    public static final String FIRST_GROUP_ID = "s-first-group-id";
+    public static final String FIRST_GROUP_ID = "first-group-id";
 }
